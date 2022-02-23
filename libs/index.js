2dec3e6b4f13055b1a65720ba09cd01d5118954e
Pixelbo <pixelbo21@gmail.com>
2022-02-23 12:45:32 +0100

    better css and file hierachy

--- libs/index.js
+++ libs/index.js
@@ -1,3 +1,58 @@
+//This is a "clean" js file without node that serves for changing aspect of the page when buttons are pressed
+
+
+var iframe, btn_about, btn_list;
+
+
+window.addEventListener('DOMContentLoaded', () => { //Define vars
+  iframe = document.getElementById("main");
+  btn_about = document.getElementById("about");
+  btn_list = document.getElementById("list");
+});
+
+function clear_active() { //Clear colors on the game selector
+  var nodes = document.getElementById('titleSelector').childNodes;
+  for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i].nodeName.toLowerCase() == 'a') {
+      nodes[i].classList.remove("active_btn");
+    }
+  }
+}
+
+function load_common(title_id) { //Load the page that is common for every game
+  iframe.src = "./titles/common.html";
+
+  btn_list.classList.remove("active_btn");
+  btn_about.classList.remove("active_btn");
+  document.getElementById(title_id).classList.add("active_btn");
+
+  window.titleID = title_id; //Global var for the title ID
+
+}
+
+function load_list() { //Seperate function cause of the active color
+  iframe.src = "./list.html";
+  btn_list.classList.add("active_btn");
+  btn_about.classList.remove("active_btn");
+
+  clear_active();
+}
+
+function load_about() { //Seperate function cause of the active color
+  iframe.src = "./about.html";
+  btn_list.classList.remove("active_btn");
+  btn_about.classList.add("active_btn");
+
+  clear_active();
+}
+
+function iframe_reload_event() {
+  const reloadIframeEvent = new CustomEvent("reloadIframeEvent", {
+    detail: window.titleID
+  }); //New page means reaload common infos
+  window.dispatchEvent(reloadIframeEvent);
+}
+
 // Get the Sidebar
 var mySidebar = document.getElementById("mySidebar");
 
--- libs/menu_dropdown.js
+++ libs/menu_dropdown.js
@@ -9,24 +9,24 @@ window.addEventListener('DOMContentLoaded', () => { //Define vars
 function toggle_dropdown(which) {
     document.getElementById(which).classList.toggle("show");
 }
-  
+
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
+window.onclick = function (e) {
     var dropdown_input = document.getElementById("dropdown_input");
     var dropdown_output = document.getElementById("dropdown_output");
 
-    if(!e.target.matches('.dropbtnI') && !e.target.matches('.dropbtnO')){ //Cool garbage that works
+    if (!e.target.matches('.dropbtnI') && !e.target.matches('.dropbtnO')) { //Cool garbage that works
         if (dropdown_input.classList.contains('show')) {
             dropdown_input.classList.remove('show');
         }
         if (dropdown_output.classList.contains('show')) {
             dropdown_output.classList.remove('show');
         }
-    }else if (!e.target.matches('.dropbtnI')) {
+    } else if (!e.target.matches('.dropbtnI')) {
         if (dropdown_input.classList.contains('show')) {
             dropdown_input.classList.remove('show');
         }
-    }else if(!e.target.matches('.dropbtnO')){
+    } else if (!e.target.matches('.dropbtnO')) {
         if (dropdown_output.classList.contains('show')) {
             dropdown_output.classList.remove('show');
         }
@@ -34,15 +34,17 @@ window.onclick = function(e) {
 }
 
 function load_game(ID) { //load the page that is specific to the game
-    iframe.src = `./titles/${ID}.html`;
+    iframe.src = `./titles/${ID}/${ID}.html`;
 }
 
 function load_common() { //not the same common as menu.js!
     iframe.src = `./titles/common.html`;
 }
 
-function reload_Common_Event(mess_id, input){
+function reload_Common_Event(mess_id, input) {
     var param = (input ? "I" : "O") + mess_id
-    const reloadCommonEvent = new CustomEvent("reloadCommonEvent", {detail:param});
-    window.parent.dispatchEvent(reloadCommonEvent); 
+    const reloadCommonEvent = new CustomEvent("reloadCommonEvent", {
+        detail: param
+    });
+    window.parent.dispatchEvent(reloadCommonEvent);
 }
