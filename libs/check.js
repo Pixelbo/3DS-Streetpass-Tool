db53fe84763077cf48da156418690ef2635b798b
Pixelbo <pixelbo21@gmail.com>
2022-04-08 18:42:18 +0200

    yes

--- libs/check.js
+++ libs/check.js
@@ -6,11 +6,11 @@ const clc = require("cli-color");
 function readTimestamp(fileBuffer, offset) {
     var date = new Date(
         parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x00, 0x2, true)), 16), //Year
-        parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x04, 0x1, true)), 16), //Month
+        parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x04, 0x1, true)), 16)-1, //Month -1 cause fo the month offset
         parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x05, 0x1, true)), 16), //Day
-        parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x06, 0x1, true)), 16), //Hours
-        parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x07, 0x1, true)), 16), //Minutes
-        parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x08, 0x1, true)), 16) //Seconds
+        parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x07, 0x1, true)), 16)+2, //Hours +2 temp soltution
+        parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x08, 0x1, true)), 16), //Minutes
+        parseInt(read.reverse_endian(read.readHex(fileBuffer, offset + 0x09, 0x1, true)), 16) //Seconds
     );
 
     return date;
@@ -43,12 +43,24 @@ exports.checkTitle = function (titleID, CEC_path) {
 
     var dateAccessed = readTimestamp(BoxInfoBuffer, 0x34);
     var dateReceived = readTimestamp(BoxInfoBuffer, 0x44);
-    //TODO: get hmac key?
+
+    var flag = read.reverse_endian(read.readHex(BoxInfoBuffer, 0x0C, 0x01, true)) ;
+    var flagMeaning = "";
+
+    if(flag == 0x01){flagMeaning = "APPLI";}
+    else if(flag == 0x02){flagMeaning = "SYS";}
+    else if(flag == 0x04){flagMeaning = "SYS2";}
+    else if(flag == 0x81){flagMeaning = "HIDDEN";}
+
+    var HMAC = read.reverse_endian(read.readHex(BoxInfoBuffer, 0x10, 0x20, true));
 
     var respDict = {
         BoxInfo: {
             titleID: titleID,
             titleName: titleName,
+            flag: flag,
+            flagMeaning: flagMeaning,
+            hmac: HMAC,
             dateAccessed: dateAccessed,
             dateReceived: dateReceived
         }
@@ -56,3 +68,79 @@ exports.checkTitle = function (titleID, CEC_path) {
 
     return respDict;
 };
+
+exports.checkInOutboxInfo = function(titleID, CEC_path){
+    ///////////////////////////////////////////Check inbox with InboxInfo file
+    var InboxInfoBuffer = read.load_file(CEC_path + titleID + "/InboxInfo");
+
+    if (read.readHex(InboxInfoBuffer, 0x00, 0x02, true) != "6262") { //Check magic number
+        console.log(clc.red.bold("ERROR: bad data in InboxInfo file (magic number)"));
+        return false;
+    }
+
+    let messNumI = parseInt(read.reverse_endian(read.readHex(InboxInfoBuffer, 0x14, 0x04, true)), 16);
+    
+    if( messNumI != read.listFiles(CEC_path + titleID + "/Inbox/").length){
+        console.log(clc.red.bold("ERROR: Conflict with the number of inbox files and InboxInfo"));
+        return false;
+    }
+
+    let maxMessNumI = parseInt(read.reverse_endian(read.readHex(InboxInfoBuffer, 0x10, 0x04, true)), 16);
+
+    if(messNumI>maxMessNumI){
+        console.log(clc.red.bold("ERROR: more inbox mess than the max in inbox info!"));
+        return false;
+    }
+
+    let messSizeMaxI = parseInt(read.reverse_endian(read.readHex(InboxInfoBuffer, 0x1C, 0x04, true)), 16);
+
+    let boxSizeI = parseInt(read.reverse_endian(read.readHex(InboxInfoBuffer, 0x0C, 0x04, true)), 16);
+    let maxBoxSizeI = parseInt(read.reverse_endian(read.readHex(InboxInfoBuffer, 0x08, 0x04, true)), 16);
+
+    ///////////////////////////////////////////Check outobx with OutboxInfo file
+
+    var OutboxInfoBuffer = read.load_file(CEC_path + titleID + "/OutboxInfo");
+
+    if (read.readHex(OutboxInfoBuffer, 0x00, 0x02, true) != "6262") { //Check magic number
+        console.log(clc.red.bold("ERROR: bad data in Outobx file (magic number)"));
+        return false;
+    }
+
+    let messNumO = parseInt(read.reverse_endian(read.readHex(OutboxInfoBuffer, 0x14, 0x04, true)), 16);
+    
+    if( messNumI != read.listFiles(CEC_path + titleID + "/Inbox/").length){
+        console.log(clc.red.bold("ERROR: Conflict with the number of inbox files and InboxInfo"));
+        return false;
+    }
+
+    let maxMessNumO = parseInt(read.reverse_endian(read.readHex(OutboxInfoBuffer, 0x10, 0x04, true)), 16);
+
+    if(messNumO>maxMessNumO){
+        console.log(clc.red.bold("ERROR: more inbox mess than the max in inbox info!"));
+        return false;
+    }
+
+    let messSizeMaxO = parseInt(read.reverse_endian(read.readHex(OutboxInfoBuffer, 0x1C, 0x04, true)), 16);
+
+    let boxSizeO = parseInt(read.reverse_endian(read.readHex(OutboxInfoBuffer, 0x0C, 0x04, true)), 16);
+    let maxBoxSizeO = parseInt(read.reverse_endian(read.readHex(OutboxInfoBuffer, 0x08, 0x04, true)), 16);
+
+    var respDict = {
+        InboxInfo: {
+            messNum: messNumI,
+            maxMessNum: maxMessNumI,
+            boxSize: boxSizeI,
+            maxBoxSize: maxBoxSizeI,
+            messSizeMax: messSizeMaxI
+        },
+        OutboxInfo: {
+            messNum: messNumO,
+            maxMessNum: maxMessNumO,
+            boxSize: boxSizeO,
+            maxBoxSize: maxBoxSizeO,
+            messSizeMax: messSizeMaxO
+        }
+    };
+
+    return respDict;
+}
--- main.js
+++ main.js
@@ -2,53 +2,56 @@
 This is the main file TODO: this file
 */
 
-const inquirer = require("inquirer");
+const inquirer = require("inquirer"); //Import some modules
 const clc = require("cli-color");
 var pather = require("path");
 
-const reading = require("./libs/reading");
+const reading = require("./libs/reading"); //Import own made modules
 const checker = require("./libs/check");
-const { cp } = require("fs");
 
-var CEC_path = "./CEC/";
+var CEC_path = "./CEC/"; //Default path to the cec directory
+var generalDict = {}; //All knowing dict
 
-var MainMessage = "\n";
+///////////////////////////////Message that will apear every time a prompt is here
+var MainMessage = "\n"; 
 
 MainMessage += "┌──────────────────────────────────────────────────────────┐\n";
 MainMessage +=
-    "│                    " + clc.underline.bold("3DS Streetpass Tool") + "                   │\n";
+    "│                    " + clc.green.underline.bold("3DS Streetpass Tool") + "                   │\n";
 MainMessage += "│                                                          │\n";
 MainMessage += "│                                                          │\n";
-MainMessage += "│Share love and the GitHub!                By Pixelbo, 2022│\n";
+MainMessage += "│Share love and the GitHub!                " + clc.magenta.italic("By Pixelbo") + ", 2022│\n";
 MainMessage += "└──────────────────────────────────────────────────────────┘\n";
 
-function promptPath() {
-    inquirer
+/////////////////////////////////////////////////////////////////
+
+function promptPath() {///If the CEC dir is not found, call a prompt to enter the path
+    inquirer///Inquirer stuff
         .prompt([
             {
                 type: "input",
                 name: "cec_path",
                 message:
-                    "[✖] CEC Directory not found! \n Please input the path to the CEC directory: "
+                    MainMessage + "[✖] CEC Directory not found! \n Please input the path to the CEC directory: "
             }
         ])
         .then((answers) => {
-            if (!checker.checkCECdir(answers.cec_path)) {
+            if (!checker.checkCECdir(answers.cec_path)) {//Recursive check
                 console.clear();
                 console.log(clc.red.bold("Again, directory not found please reenter the path;"));
                 promptPath();
             } else {
-                CEC_path = pather.normalize(answers.cec_path + "/");
-                main();
+                CEC_path = pather.normalize(answers.cec_path + "/");//Normalize the path + add a slash becuase folder
+                Preload();//Jump to preload
             }
         });
 }
-
-function game_select(dict) {
+////////////////////////////////////////////////////////////Select a game from our all knowing dict
+function game_select() {
     choices = [];
 
-    for (let i = 0; i < Object.keys(dict).length; i++) {
-        choices.push({ value: i, name: dict[i]["BoxInfo"]["titleName"] });
+    for (let i = 0; i < Object.keys(generalDict).length; i++) { //Parsing the dict
+        choices.push({ value: i, name: generalDict[i]["BoxInfo"]["titleName"] });
     }
 
     inquirer
@@ -61,27 +64,39 @@ function game_select(dict) {
             }
         ])
         .then((answers) => {
-            console.log(answers.game_selector);
+            var InOutboxInfo = checker.checkInOutboxInfo(generalDict[answers.game_selector]["BoxInfo"]["titleID"], CEC_path);
+
+            if(InOutboxInfo){
+                generalDict[answers.game_selector]["InOutboxInfo"] = InOutboxInfo;
+                console.log(clc.green.bold("[🗸] Inbox/Outbox check passed!"));
+                console.clear();
+                MainMenu(answers.game_selector);
+            }else {
+                console.log(clc.red.bold(`[✖] A game as an error in his data!`));
+                return false;
+            }
+            
+            
         });
 }
-
-function main() {
+////////////////////////////////////////////////////Preaload everything
+function Preload() {
     console.clear();
 
-    if (!checker.checkCECdir(CEC_path)) {
+    if (!checker.checkCECdir(CEC_path)) { //Check the CEC Dir
         promptPath();
         return;
     } else {
         console.log(clc.green.bold("[🗸] CEC Directory found!"));
     }
 
-    var generalDict = {};
 
-    var titleCount = 0;
+
+    var titleCount = 0;///////////////For each title, check if the box is valid (id, name, etc) It does not verify input/output messages
     reading.listDirs(CEC_path).forEach((titleID) => {
         var BoxInfo = checker.checkTitle(titleID, CEC_path);
         if (BoxInfo) {
-            generalDict[titleCount] = BoxInfo;
+            generalDict[titleCount] = BoxInfo
             titleCount = titleCount + 1;
         } else {
             console.log(clc.red.bold(`[✖] A game as an error in his data!`));
@@ -89,9 +104,91 @@ function main() {
     });
     titleCount = titleCount - 1;
 
-    console.log(clc.green.bold(`[🗸] Found ${titleCount + 1} game(s)`));
+    console.log(clc.green.bold(`[🗸] Found ${titleCount + 1} game(s)`));//Prints a little message
 
-    game_select(generalDict);
+    game_select();//Select a game
 }
 
-main();
+//////////////////////////////////////////////////////General info about the game
+function GeneralInfo(listID) {
+    console.clear();
+
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "general_info",
+            message: MainMessage + "You can here view and edit (press enter) some genral info about the game!",
+            choices: [
+                {value:0, name: ("ID of the title: " + generalDict[listID]["BoxInfo"]["titleID"])},
+                {value:1, name: ("Name of the title: " + generalDict[listID]["BoxInfo"]["titleName"])},
+                {value:2, name: ("Flag: " + generalDict[listID]["BoxInfo"]["flagMeaning"])},
+                {value:3, name: ("HMAC (can't edit): " + generalDict[listID]["BoxInfo"]["hmac"])},
+                {value:4, name: ("Date Accessed: " + generalDict[listID]["BoxInfo"]["dateAccessed"])},
+                {value:5, name: ("Date Received: " + generalDict[listID]["BoxInfo"]["dateReceived"])},
+                {value:-1, name: "Get back to the main menu!"}
+        ]
+        }
+    ])
+    .then((answers) => {
+        let resp = answers.general_info;
+
+        if(resp==-1){
+            MainMenu(listID);
+        }
+    });
+
+}
+
+//////////////////////////////////////////////////////This is where the fun begins
+function MainMenu(listID){//Takes the id from the dict
+    console.clear();
+
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "main_menu",
+                message: MainMessage + "Welcome to the main menu!",
+                choices: [
+                    {value:0, name:"View/edit general info about the game"}, //todo: just display general dict info + menu to edit
+                    {value:-1, name:new inquirer.Separator()},
+                    {value:1, name:"View/edit Inbox Info"}, //todo: just display general dict info + menu to edit
+                    {value:2, name:"View/edit Outbox Info"}, //todo: just display general dict info + menu to edit
+                    {value:-1, name:new inquirer.Separator()},
+                    {value:3, name:"View/edit an inbox message"},//not for now
+                    {value:4, name:"View/edit an outbox message"},//not for now
+                    {value:5, name:"Save the edits!"},//not for now
+            ]
+            }
+        ])
+        .then((answers) => {
+            let resp = answers.main_menu;
+
+            if(resp == 0){
+                GeneralInfo(listID);
+            }else if(resp == 1){
+                console.clear();
+                console.log(clc.green.bold("[🗸] Inbox info"));
+                console.log(generalDict[listID]["InOutboxInfo"]["InboxInfo"]);
+            }else if(resp == 2){
+                console.clear();
+                console.log(clc.green.bold("[🗸] Outbox info"));
+                console.log(generalDict[listID]["InOutboxInfo"]["OutboxInfo"]);
+            }else if(resp == 3){
+                console.clear();
+                console.log(clc.green.bold("[🗸] Inbox message"));
+                console.log(generalDict[listID]["InOutboxInfo"]["InboxInfo"]["messages"][0]);
+            }else if(resp == 4){
+                console.clear();
+                console.log(clc.green.bold("[🗸] Outbox message"));
+                console.log(generalDict[listID]["InOutboxInfo"]["OutboxInfo"]["messages"][0]);
+            }else if(resp==-1){
+                console.clear();
+                console.log(clc.cyan.bold(`[?] Bro don't select the seperator`));
+                MainMenu(listID);
+            }
+        });
+}
+
+Preload();
